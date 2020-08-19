d8c77eedbea3fb2f16b9eaea7a1d0ef92fe931c2
wilmerterrero <wilmerterrero1922@gmail.com>
2020-08-18 22:44:07 -0400

    (001) - Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./pages/header/header";
+import Employees from "./examples/employees";
+import ExampleList from "./pages/pagination/pagination";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/polimorfismo" component={Employees} />
+          <ExampleList />
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
